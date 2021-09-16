01d3aa8f86fed1a79cf0965567323dad33c381ad
WIjesinghe W.A.K.D <it18111316@my.sliit.lk>
2021-09-16 23:17:10 +0530

    Added login logout Uis

--- app/src/main/java/com/example/farmory/SignUpFragment.kt
+++ app/src/main/java/com/example/farmory/SignUpFragment.kt
@@ -8,17 +8,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
-class LogInFragment : Fragment() {
-
+class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_log_in, container, false)
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
-        view.findViewById<TextView>(R.id.signUpHereBtn).setOnClickListener{
-            findNavController().navigate(R.id.action_logInFragment_to_signUpFragment)
+        view.findViewById<TextView>(R.id.signInHereBtn).setOnClickListener{
+            findNavController().navigate(R.id.action_signUpFragment_to_logInFragment)
         }
 
         return view
